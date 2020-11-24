d27d76f8829257d2350b54abfc66d7c615f04cb1
Ozan <bakonong99@gmail.com>
2020-11-24 23:36:24 +0700

    Latihan JSON

---
+++ latihan1.php
@@ -0,0 +1,15 @@
+<?php 
+
+	$mahasiswa = [
+		"nama" => "Rizqi Fauzan",
+		"nrp" => "173040150",
+		"email" => "fauzanrizqi243@gmail.com",
+		"nama" => "Erik Doank",
+		"nrp" => "023040001",
+		"email" => "erik@gmail.com"
+ 	];
+
+ 	$data = json_encode($mahasiswa);
+ 	echo $data;
+
+ ?>
